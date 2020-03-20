739d588edf7ee336c9914220cea66e8f6b304a2c
Blade Barringer <blade@getbraintree.com>
2020-03-20 17:03:44 -0500

    Migrate to jest (#103)
    * Migrate to jest
    
    closes #102
    
    * Apply suggestions from code review
    
    Co-Authored-By: Aki <aki.braun@getbraintree.com>
    
    Co-authored-by: Aki <aki.braun@getbraintree.com>

---
+++ __tests__/find-best-match.js
@@ -0,0 +1,34 @@
+const findBestMatch = require('../lib/find-best-match');
+
+describe('findBestMatch', () => {
+  it('returns nothing if there are not enough results to try to match', () => {
+    expect(findBestMatch([])).toBeUndefined();
+  });
+
+  it('returns nothing if not every element has a matchStrength property', () => {
+    expect(findBestMatch([{
+      matchStrength: 4
+    }, {
+    }, {
+      matchStrength: 5
+    }])).toBeUndefined();
+  });
+
+  it('returns the result with the greatest matchStrength value', () => {
+    const a = {
+      matchStrength: 4
+    };
+    const b = {
+      matchStrength: 1
+    };
+    const c = {
+      matchStrength: 40
+    };
+    const d = {
+      matchStrength: 7
+    };
+    const results = [a, b, c, d];
+
+    expect(findBestMatch(results)).toBe(c);
+  });
+});
