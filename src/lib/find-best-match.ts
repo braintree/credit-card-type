8466f47a7c2704cd7c5da4cfb224d72cc2d0fa01
Blade Barringer <blade.barringer@getbraintree.com>
2020-04-21 21:07:10 +0000

    Use Number() to coerce

--- src/lib/find-best-match.ts
+++ src/lib/find-best-match.ts
@@ -30,14 +30,11 @@ export default function findBestMatch(
       return result;
     }
 
-    const currentMatchStrength = bestMatch.matchStrength as number;
-    const newMatchStrength = result.matchStrength as number;
-
     /*
      * If the current best match pattern is less specific than this result, set
      * the result as the new best match
      * */
-    if (currentMatchStrength < newMatchStrength) {
+    if (Number(bestMatch.matchStrength) < Number(result.matchStrength)) {
       return result;
     }
 
