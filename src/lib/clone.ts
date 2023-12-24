0f83811928c6e63b375d8685ebdcbf32605aa215
Ron Strauss <82303541+RonStrauss@users.noreply.github.com>
2023-12-24 13:14:07 +0200

    add support for 'structuredClone' deep cloning
    https://developer.mozilla.org/en-US/docs/Web/API/structuredClone

--- src/lib/clone.ts
+++ src/lib/clone.ts
@@ -1,6 +1,11 @@
+const hasStructedClone = 'structuredClone' in globalThis;
 export function clone<T>(originalObject: T): T | null {
   if (!originalObject) {
     return null;
+  }
+
+  if (hasStructedClone){
+    return structedClone(originalObject);
   }
 
   return JSON.parse(JSON.stringify(originalObject));
